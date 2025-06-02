add4127052ac1531071ffd1270d52b529f43dedf
Michael Haley <mrmichaelhaley@gmail.com>
2025-06-02 16:10:53 +0100

    Created package

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wsi-tools',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'dask[array]',
+        'scikit-image',
+        'ome-types',
+        'zarr',
+        'dask-image',
+        'scipy',
+        'napari',
+        'magicgui',
+        'qtpy',
+        'distributed',  # for dask.distributed
+    ],
+    description='Tools for analysing whole slide imaging datasets',
+    author='Michael Haley',
+    author_email='mrmichaelhaley@gmail.com',
+    url='https://github.com/dr-michael-haley/wsi-tools',
+)
